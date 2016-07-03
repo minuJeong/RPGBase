45a529ea1911b48f4ff9b4b1d0eed8ec7b335f3b
Minu Jeong <mjung@ea.com>
2016-07-03 16:04:01 +0900

    add new pawn type

---
+++ Assets/Resources_Static/Pawn/TestPawn/PlayerPawn_View_AnimParams.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerPawn_View_AnimParams : MonoBehaviour
+{
+	Animator _Mecanim;
+	Vector3 _LastPosition;
+
+	// Use this for initialization
+	void Start ()
+	{
+		_Mecanim = GetComponent<Animator> ();
+		Debug.Assert (_Mecanim != null);
+
+		_UpdatePosition ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		_UpdateSpeed ();
+		_UpdatePosition ();
+	}
+
+	void _UpdateSpeed ()
+	{
+		Vector3 delta = transform.position - _LastPosition;
+		_Mecanim.SetFloat ("MoveSpeed", delta.magnitude / Time.deltaTime);
+		Debug.Log (delta.magnitude / Time.deltaTime);
+	}
+
+	void _UpdatePosition ()
+	{
+		_LastPosition = transform.position;
+	}
+}
